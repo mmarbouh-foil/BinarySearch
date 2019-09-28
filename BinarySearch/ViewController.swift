208c4454634d013aca10e1d1ace456c3f95b7415
Mohamed Marbouh <mohamedm@avenza.com>
2019-09-27 21:12:17 -0400

    Initial commit

--- BinarySearch/ViewController.swift
+++ BinarySearch/ViewController.swift
@@ -8,13 +8,44 @@
 
 import UIKit
 
+func search(list: [Int], for value: Int) -> Int? {
+    var lowerIndex = 0
+    var upperIndex = list.count - 1
+    
+    while true {
+        let index = (lowerIndex + upperIndex)/2
+        
+        if list[index] == value {
+            return index
+        } else if lowerIndex > upperIndex {
+            return nil
+        } else {
+            if list[index] > value {
+                upperIndex = index - 1
+            } else {
+                lowerIndex = index + 1
+            }
+        }
+    }
+}
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let resultsLabel = UILabel()
+        resultsLabel.center = CGPoint(x: 20, y: 200)
+        view.addSubview(resultsLabel)
+        
+        let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        let challenge = Int.random(in: 0..<10)
+        
+        if let result = search(list: numbers, for: challenge) {
+            resultsLabel.text = "Found \(challenge) at index \(result) in \(numbers)"
+            resultsLabel.sizeToFit()
+        }
     }
-
 
 }
 
